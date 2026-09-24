0c8e16e83f8dd976f5f9fc540ca0b03b2022f8b3
telling7895-eng <telling7895@gmail.com>
2026-09-24 23:21:00 +0900

    fix: 알림 Clock 빈 중복 등록 해소 (#51)

--- src/main/java/com/dongbang/notification/infrastructure/NotificationConfiguration.java
+++ src/main/java/com/dongbang/notification/infrastructure/NotificationConfiguration.java
@@ -1,17 +1,9 @@
 package com.dongbang.notification.infrastructure;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.time.Clock;
 
 @Configuration
 @EnableScheduling
 public class NotificationConfiguration {
-
-    @Bean
-    Clock applicationClock() {
-        return Clock.systemUTC();
-    }
 }
